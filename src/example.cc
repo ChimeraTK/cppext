87bb4d8dedb7279126bdffff75d906ba4516354d
Martin Hierholzer <martin.hierholzer@desy.de>
2018-04-16 14:58:40 +0200

    added example (from README.md)

---
+++ src/example.cc
@@ -0,0 +1,31 @@
+#include <thread>
+#include <future_queue.hpp>
+#include <iostream>
+#include <unistd.h>
+
+// define future_queue of doubles with a length of 5
+future_queue<double> myQueue(5);
+
+// define function sending data in a separate thread
+void senderThread() {
+  for(size_t i=0; i<10; ++i) {
+    usleep(100000);             // wait 0.1 second
+    myQueue.push(i*3.14);
+  }
+}
+
+int main() {
+
+  // launch sender thread
+  std::thread myThread(&senderThread);
+
+  // receive 10 values and print them
+  for(size_t i=0; i<10; ++i) {
+    double value;
+    myQueue.pop_wait(value);                // wait until new data has arrived
+    std::cout << value << std::endl;
+  }
+  
+  myThread.join();
+  return 0;
+}

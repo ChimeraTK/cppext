6809da1d19c3de6c15ad65068a52f98f3f779ae2
Martin Hierholzer <martin.hierholzer@desy.de>
2020-04-15 17:17:57 +0200

    added missing namespace for cppext::finally

--- include/finally.hpp
+++ include/finally.hpp
@@ -6,33 +6,37 @@
 // Taken from GSL library, https://github.com/Microsoft/GSL/blob/master/include/gsl/gsl_util
 // Original Licence: MIT
 
-template<class F>
-class final_act {
- public:
-  explicit final_act(F f) noexcept : f_(std::move(f)), invoke_(true) {}
+namespace cppext {
 
-  final_act(final_act&& other) noexcept : f_(std::move(other.f_)), invoke_(other.invoke_) { other.invoke_ = false; }
+  template<class F>
+  class final_act {
+   public:
+    explicit final_act(F f) noexcept : f_(std::move(f)), invoke_(true) {}
 
-  final_act(const final_act&) = delete;
-  final_act& operator=(const final_act&) = delete;
+    final_act(final_act&& other) noexcept : f_(std::move(other.f_)), invoke_(other.invoke_) { other.invoke_ = false; }
 
-  ~final_act() noexcept {
-    if(invoke_) f_();
+    final_act(const final_act&) = delete;
+    final_act& operator=(const final_act&) = delete;
+
+    ~final_act() noexcept {
+      if(invoke_) f_();
+    }
+
+   private:
+    F f_;
+    bool invoke_;
+  };
+
+  template<class F>
+  inline final_act<F> finally(const F& f) noexcept {
+    return final_act<F>(f);
   }
 
- private:
-  F f_;
-  bool invoke_;
-};
+  template<class F>
+  inline final_act<F> finally(F&& f) noexcept {
+    return final_act<F>(std::forward<F>(f));
+  }
 
-template<class F>
-inline final_act<F> finally(const F& f) noexcept {
-  return final_act<F>(f);
-}
-
-template<class F>
-inline final_act<F> finally(F&& f) noexcept {
-  return final_act<F>(std::forward<F>(f));
-}
+} // namespace cppext
 
 #endif // CPPEXT_FINALLY_HPP
